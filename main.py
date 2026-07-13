33306ba43a182463a1bd47f5e1f3d75ff4050b3b
Ashraf Hussain <ashrafsen2@gmail.com>
2026-07-13 20:18:09 +0600

    change from feature/change-greeting

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-app_color = "Red"
+app_color = "violet"
 
 
 @app.route("/")
 def hello():
-    return "Hello from devops"
+    return "Hello from feature/change-greeting"
 
 
 @app.route("/user/<username>")
